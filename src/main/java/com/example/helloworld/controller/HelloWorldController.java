aabe8768249b104bc00b0fd05cb1670bae737fab
Fabio Cerri <fabiocerri@Fabios-MacBook-Pro-2.local>
2023-09-05 10:58:00 +0200

    Initial commit

--- src/main/java/com/example/helloworld/controller/HelloWorldController.java
+++ src/main/java/com/example/helloworld/controller/HelloWorldController.java
@@ -15,9 +15,9 @@ public class HelloWorldController {
     @GetMapping("ping")
     public String ping(){
         // Replace these values with your Redis server configuration
-        String redisHost = "localhost"; // Redis server host
+        String redisHost = "127.0.0.1"; // Redis server host
         int redisPort = 6379;          // Redis server port
-        String redisPassword = "your_password"; // Replace with your Redis password
+        String redisPassword = "Strong0Psw0"; // Replace with your Redis password
 
         // Create a Jedis instance
         Jedis jedis = new Jedis(redisHost, redisPort);
