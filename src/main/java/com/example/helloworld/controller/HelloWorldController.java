26cd365f383d35b1fac780b0df66fb61ca13e868
Fabio Cerri <fabiocerri@Fabios-MacBook-Pro-2.local>
2023-09-05 10:55:58 +0200

    Initial commit

--- src/main/java/com/example/helloworld/controller/HelloWorldController.java
+++ src/main/java/com/example/helloworld/controller/HelloWorldController.java
@@ -1,5 +1,7 @@
 package com.example.helloworld.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 import redis.clients.jedis.Jedis;
 
 @RestController
